e9e62933d3190b72347a51d52a6f2dc26b58c81f
Yingzhengxinjianchong <2219704394@qq.com>
2026-03-19 11:11:36 +0800

    修复部分内容

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // 配置 @ 别名指向 src 目录，方便后续深层级目录引入文件
-      '@': path.resolve(__dirname, 'src') 
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
